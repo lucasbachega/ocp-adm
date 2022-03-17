27536529da8d62aaddb141b7e9bec689bc9b8e35
Lucas Bachega <lucas.cbachega@gmail.com>
2022-03-17 13:42:10 -0300

    fab button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Logo from '../src/assets/soufer-logo.png'
 import Table from './components/Table';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   return (
@@ -8,9 +10,13 @@ function App() {
       <header className='header'>
         <img src={Logo} className='logo' width={150} />
         <p className='title'>PAINEL DO ADMINISTRADOR - OCP</p>
+        <Fab size='large' variant="extended" style={{ position: 'absolute', backgroundColor: '#EE3236', color: 'white' }} className='fab' aria-label="add">
+          <AddIcon style={{marginRight: '10px'}}/>
+          Novo usuário
+        </Fab>
       </header>
       <div className='content'>
-          <Table />
+        <Table />
       </div>
     </div>
   );
